77eef48869bdbfbb73cd2f72dbee4cd2e3d46cdb
shamsnaamir <shamsnaamir@gmail.com>
2022-01-26 18:05:36 +0330

    init

---
+++ server.js
@@ -0,0 +1,41 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api/:year-:mounth-:day', function (req, res) {
+  let date = new Date( req.params.year+" "+req.params.mounth+" "+req.params.day+" gmt");
+  let ts = date.getTime();
+  res.send({unix:ts,utc:date.toUTCString()})
+})
+
+app.get('/api/:timestamp', function (req, res) {
+  let date = new Date(+req.params.timestamp);
+  res.send({unix:+req.params.timestamp,utc:date.toUTCString()})
+})
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
